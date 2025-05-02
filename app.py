3ea9d949282e3e6a6609aee9dd946afc405c38c4
amirda88 <amir.davarpanah@valsamis.com>
2025-05-02 16:20:29 -0500

    app.py

--- app.py
+++ app.py
@@ -431,24 +431,24 @@ def report_filter_page():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-   if request.method == 'POST':
-      username = request.form['username']
-      password = request.form['password']
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-      # ✅ Assign roles to each user
-      # ✅ Check user in database
-      user = User.query.filter_by(username=username, password=password).first()
-      if user:
-             session['logged_in'] = True
-         session['username'] = user.username
-             session['role'] = user.role
-             session['permissions'] = user.permissions.split(',') if user.permissions else []
-             return redirect(url_for('home'))
+        # ✅ Assign roles to each user
+        # ✅ Check user in database
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+                session['logged_in'] = True
+            session['username'] = user.username
+                session['role'] = user.role
+                session['permissions'] = user.permissions.split(',') if user.permissions else []
+                return redirect(url_for('home'))
 
-      else:
-         return render_template('login.html', error='Invalid username or password.')
+        else:
+            return render_template('login.html', error='Invalid username or password.')
 
-   return render_template('login.html')
+    return render_template('login.html')
 
 
 @app.route('/history/<int:cylinder_id>')
